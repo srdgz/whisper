af307e5bf4a6a8efc6d8239ac0d143198753d9f1
srdgz <sandra.rr@icloud.com>
2024-08-30 11:26:33 +0200

    I add an add button to the home view

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import ChatList from "../components/ChatList";
 import Loading from "../components/Loading";
+import Octicons from "@expo/vector-icons/Octicons";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -15,6 +16,8 @@ import { getDocs, query, where } from "firebase/firestore";
 const Home: React.FC = () => {
   const { user, logout } = useAuth();
   const [users, setUsers] = useState<User[]>([]);
+  const [isSearchUserModalVisible, setIsSearchUserModalVisible] =
+    useState<boolean>(false);
 
   const getUsers = async () => {
     if (!user?.id) return;
@@ -44,6 +47,17 @@ const Home: React.FC = () => {
           <Loading size={hp(10)} />
         </View>
       )}
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          bottom: hp(3),
+          right: wp(8),
+          zIndex: 10,
+        }}
+        onPress={() => setIsSearchUserModalVisible(true)}
+      >
+        <Octicons name="plus-circle" size={hp(7)} color="#4DB6AC" />
+      </TouchableOpacity>
     </View>
   );
 };
