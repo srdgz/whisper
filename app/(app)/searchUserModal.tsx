3fd4bbb3eea3da81e1d3f7692e189b94cb71f9c9
srdgz <sandra.rr@icloud.com>
2024-09-11 16:52:22 +0200

    feat: add input cleanup in the search user modal

--- app/(app)/searchUserModal.tsx
+++ app/(app)/searchUserModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Modal, View, Text, TextInput, Pressable } from "react-native";
 import { SearchUserModalProps } from "../constants/types";
 
@@ -10,12 +10,25 @@ const SearchUserModal: React.FC<SearchUserModalProps> = ({
   onSearch,
   onClose,
 }) => {
+  const [localEmail, setLocalEmail] = useState(email);
+
+  const handleClose = () => {
+    setEmail(localEmail);
+    onClose();
+  };
+
+  useEffect(() => {
+    if (visible) {
+      setLocalEmail("");
+    }
+  }, [visible]);
+
   return (
     <Modal
       transparent={true}
       visible={visible}
       animationType="slide"
-      onRequestClose={onClose}
+      onRequestClose={handleClose}
     >
       <View className="flex-1 justify-center items-center bg-gray-200 bg-opacity-50">
         <View className="bg-white p-8 rounded-lg w-11/12">
@@ -24,8 +37,8 @@ const SearchUserModal: React.FC<SearchUserModalProps> = ({
           </Text>
           <TextInput
             placeholder="Introduce el email"
-            value={email}
-            onChangeText={setEmail}
+            value={localEmail}
+            onChangeText={setLocalEmail}
             className="border p-3 rounded-md border-teal-400 mb-4"
             keyboardType="email-address"
           />
@@ -39,7 +52,7 @@ const SearchUserModal: React.FC<SearchUserModalProps> = ({
             </Text>
           </Pressable>
           <Pressable
-            onPress={onClose}
+            onPress={handleClose}
             className="mt-6 p-2 bg-gray-300 rounded-md"
           >
             <Text className="text-center text-gray-700 text-xl">Cancelar</Text>
